f117414ad82bdd0f3fada8f6fced69163b08d311
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-18 18:37:07 +0700

    add config

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const App = require('./src/app.js');
+const config = require('./config');
 
-const app = new App();
+const app = new App({
+  env: config.env,
+  port: config.app.port,
+});
 
 app.run();
