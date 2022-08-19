159b95739fb4d29c900a16264e5be7e08259f8ef
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-19 17:29:24 +0700

    add db config

--- config/development.config.js
+++ config/development.config.js
@@ -7,4 +7,15 @@ module.exports = {
     url: process.env.APP_URL || 'http://localhost:4000',
     key: process.env.APP_KEY || 'secret',
   },
+  auth: {
+    accessTokenExpire: process.env.ACCESS_TOKEN_EXPIRE || '15m',
+    refreshTokenExpire: process.env.REFRESH_TOKEN_EXPIRE || '30m',
+  },
+  db: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'noto-server',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
 };
--- config/production.config.js
+++ config/production.config.js
@@ -5,4 +5,15 @@ module.exports = {
     url: process.env.APP_URL || 'http://test.com',
     key: process.env.APP_KEY || 'secret',
   },
+  auth: {
+    accessTokenExpire: process.env.ACCESS_TOKEN_EXPIRE || '15m',
+    refreshTokenExpire: process.env.REFRESH_TOKEN_EXPIRE || '30m',
+  },
+  db: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'rootpass',
+    database: process.env.DB_DATABASE || 'noto-server',
+    host: process.env.DB_HOST || 'http://test.com',
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
 };
