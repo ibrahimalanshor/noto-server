a272bed4f0f41e408100bab6593c068910268b6f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-20 07:57:56 +0700

    add conflict exception

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -2,4 +2,6 @@ module.exports = {
   HttpException: require('./http.exception.js'),
   UnprocessableEntityException: require('./unprocessable-entity.exception.js'),
   ConflictException: require('./conflict.exception.js'),
+  NotFoundException: require('./not-found.exception.js'),
+  UnauthorizedException: require('./unauthorized.exception.js'),
 };
