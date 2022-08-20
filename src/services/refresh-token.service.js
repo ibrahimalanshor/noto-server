0fa938e920ff41234aea648fc524bfb737aba8ae
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-20 07:59:42 +0700

    refresh token delete by user id

--- src/services/refresh-token.service.js
+++ src/services/refresh-token.service.js
@@ -2,10 +2,12 @@ const crypto = require('crypto');
 
 function createRefreshTokenService({ refreshTokenRepository }) {
   async function createRefreshToken(userId) {
+    await refreshTokenRepository.deleteByUserId(userId);
+
     return await refreshTokenRepository.create({
       userId,
       token: crypto.randomBytes(32).toString('hex'),
-      expiresIn: new Date(Date.now() + 1000 * 864000 * 30),
+      expiresIn: new Date(Date.now() + 1000 * 86400 * 30),
     });
   }
 
