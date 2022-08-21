cc678c5d6421b3c1e4e54e3d50b6c4cc2473b807
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-21 15:41:14 +0700

    add access token validator helper

---
+++ src/helpers/access-token-validator.helper.js
@@ -0,0 +1,21 @@
+const { UnauthorizedException } = require('../exceptions');
+
+function createAccessTokenValidator({ tokenService, userService }) {
+  return async (req, res, next) => {
+    try {
+      if (!req.headers.authorization) throw new Error();
+
+      const auth = await tokenService.verify(req.headers.authorization);
+      const user = await userService.findById(auth.id);
+
+      req.auth = auth;
+      req.user = user;
+
+      next();
+    } catch (err) {
+      next(new UnauthorizedException(err));
+    }
+  };
+}
+
+module.exports = createAccessTokenValidator;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   createRequestValidator: require('./request-validator.helper.js'),
+  createAccessTokenValidator: require('./access-token-validator.helper.js'),
 };
