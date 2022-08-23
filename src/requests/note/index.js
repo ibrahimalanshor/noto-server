69e02ab98c1ccb25257041a06a8dcc3faa814669
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-23 19:59:25 +0700

    add note update trash request

--- src/requests/note/index.js
+++ src/requests/note/index.js
@@ -2,4 +2,5 @@ module.exports = {
   createNoteCreateRequest: require('./create.request.js'),
   createNoteUpdateRequest: require('./update.request.js'),
   createNoteUpdateFavoriteRequest: require('./update-favorite.request.js'),
+  createNoteUpdateTrashRequest: require('./update-trash.request.js'),
 };
