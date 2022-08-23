ec451c82abd6db8576392d8fe36ef003a90b5f71
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-23 08:53:23 +0700

    add user can access tag policy

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -1,3 +1,8 @@
 const UserSchema = require('./user.schema.js');
+const { isForbidden } = require('../../utils/database');
+
+UserSchema.prototype.canAccessTag = function (tag) {
+  isForbidden(tag.userId !== this.id);
+};
 
 module.exports = UserSchema;
