ce510c73fdbff45fe2d96f5511013d226411face
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-23 18:41:08 +0700

    add user can access note policy

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -5,4 +5,8 @@ UserSchema.prototype.canAccessTag = function (tag) {
   isForbidden(tag.userId !== this.id);
 };
 
+UserSchema.prototype.canAccessNote = function (note) {
+  isForbidden(note.userId !== this.id);
+};
+
 module.exports = UserSchema;
