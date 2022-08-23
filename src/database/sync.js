e81455624806c4dbb301b752047c9a98180683d8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-23 09:15:48 +0700

    add note table sync

--- src/database/sync.js
+++ src/database/sync.js
@@ -2,6 +2,7 @@ const sequelize = require('./sequelize.js');
 const { UserModel } = require('../models/user');
 const { RefreshTokenModel } = require('../models/refresh-token');
 const { TagModel } = require('../models/tag');
+const { NoteModel } = require('../models/note');
 
 async function run() {
   try {
@@ -10,6 +11,7 @@ async function run() {
     await UserModel.sync({ force: true });
     await RefreshTokenModel.sync({ force: true });
     await TagModel.sync({ force: true });
+    await NoteModel.sync({ force: true });
   } catch (err) {
     console.error(err);
   } finally {
