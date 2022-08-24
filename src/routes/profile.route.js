5fdf4ff98acca77f768ef3b56339d4c5291e3f2e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-25 05:59:12 +0700

    add update profile route

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,13 +1,29 @@
 const { requireAuth } = require('../middlewares');
+const { createRequestValidator } = require('../helpers');
+
+const { UserModel } = require('../models/user');
+const { createUserRepository } = require('../repositories');
 
 const { createProfileService } = require('../services');
 const { createProfileController } = require('../controllers');
 
+const { createProfileUpdateRequest } = require('../requests/profile');
+
 function createProfileRoute(router) {
-  const profileService = createProfileService({});
+  const userRepository = createUserRepository({ userModel: UserModel });
+
+  const profileService = createProfileService({ userRepository });
   const profileController = createProfileController({ profileService });
 
+  const profileUpdateRequest = createProfileUpdateRequest();
+
   router.get('/profile', requireAuth, profileController.getProfile);
+  router.patch(
+    '/profile',
+    requireAuth,
+    createRequestValidator(profileUpdateRequest.rules),
+    profileController.updateProfile
+  );
 }
 
 module.exports = createProfileRoute;
